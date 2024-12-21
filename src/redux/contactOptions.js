97e42e4639574b6a9cef286b6d21976bcea95ad4
rojhatbalta <rojhatbalta@gmail.com>
2024-12-21 20:38:23 +0300

    get list

--- src/redux/contactOptions.js
+++ src/redux/contactOptions.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import Axios from "axios";
 
-const URL = import.meta.env.VITE_URL;
+const URL = "https://676692c7410f849996580554.mockapi.io/contacts";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
