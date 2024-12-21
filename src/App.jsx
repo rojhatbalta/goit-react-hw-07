9023aa3e3b9e08db24d08a170558ec8f87319678
rojhatbalta <rojhatbalta@gmail.com>
2024-12-21 20:34:56 +0300

    get list

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,19 @@ import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 import Style from "./App.module.css";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchContacts } from "./redux/contactOptions";
 
 export default function App() {
+  const dispatch = useDispatch();
+  useEffect(
+    function () {
+      dispatch(fetchContacts());
+    },
+    [dispatch]
+  );
+
   return (
     <>
       <h1>Phonebook</h1>
